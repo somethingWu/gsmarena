1a58dc194919db8d420a90fb77ad10b6723cb681
SomethingWu <F4Foxy@163.com>
2019-08-03 01:02:34 +0800

    upload

--- gsmarena/spiders/DeviceSpider.py
+++ gsmarena/spiders/DeviceSpider.py
@@ -4,13 +4,32 @@ import scrapy
 
 class DevicespiderSpider(scrapy.Spider):
     name = 'DeviceSpider'
-    allowed_domains = ['www.gsmarena.com/makers.php3']
+    allowed_domains = ['www.gsmarena.com']
     start_urls = ['http://www.gsmarena.com/makers.php3/']
 
     def parse(self, response):
-        print("hello!")
         brands = response.css(".st-text a")
         for brand in brands:
             brandname = brand.css("a::text").extract()
-            brandurl = brand.css("a::attr(href)").extract()
-            print(brandname[0] + "->" + brandurl[0])
+            brand_page = brand.css("a::attr(href)").extract()
+            if brand_page is not None:
+                brand_page = response.url.replace("makers.php3/", brand_page[0])
+                request = scrapy.Request(brand_page, meta={'brand': brandname[0]}, callback=self.deviceListParse)
+                yield request
+
+    def deviceListParse(self, response):
+        devicelist = response.css("div.makers a")
+        brand = response.meta['brand']
+        #print(brand)
+        for device in devicelist:
+            device_page = device.css("a::attr(href)").extract()
+            device_page = response.urljoin(device_page[0])
+            #print(device_page)
+            yield scrapy.Request(device_page, meta={'brand': brand}, callback=self.deviceParse)
+
+    def deviceParse(self, response):
+        deviceInfo = response.css("div#specs-list").extract()
+        deviceName = response.css("h1.specs-phone-name-title ::text").extract()
+        brand = response.meta['brand']
+        for tr in deviceInfo:
+            print(tr)
