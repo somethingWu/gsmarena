1be67de26c616ce7cc621c5395dc209c309c6aaf
SomethingWu <F4Foxy@163.com>
2019-08-04 01:03:34 +0800

    save to json file

--- gsmarena/spiders/DeviceSpider.py
+++ gsmarena/spiders/DeviceSpider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class DevicespiderSpider(scrapy.Spider):
@@ -16,7 +17,6 @@ class DevicespiderSpider(scrapy.Spider):
                 brand_page = response.url.replace("makers.php3/", brand_page[0])
                 request = scrapy.Request(brand_page, meta={'brand': brandname[0]}, callback=self.deviceListParse)
                 yield request
-                break
 
     def deviceListParse(self, response):
         devicelist = response.css("div.makers a")
@@ -27,16 +27,38 @@ class DevicespiderSpider(scrapy.Spider):
             device_page = response.urljoin(device_page[0])
             #print(device_page)
             yield scrapy.Request(device_page, meta={'brand': brand}, callback=self.deviceParse)
-            break
+        next_page = response.css("a.pages-next ::attr(href)").extract()
+        if next_page:
+            next_page = response.urljoin(next_page[0])
+            yield scrapy.Request(next_page, meta={'brand': brand}, callback=self.deviceListParse)
 
     def deviceParse(self, response):
+        global date
+        date = {}
         deviceInfo = response.css("div#specs-list table")
         deviceName = response.css("h1.specs-phone-name-title ::text").extract()
         brand = response.meta['brand']
-        date = {'brand' : brand, 'deviceNo' : deviceName}
+#        date = {'brand': brand, 'deviceNo': deviceName[0]}
+        date['brand'] = brand
+        if deviceName:
+            date['deviceNo'] = deviceName[0]
         for table in deviceInfo:
             tit = table.css("th::text").extract()
             date[tit[0]] = {}
-            print(deviceName)
-            print(table.css("*").extract())
-            break
+            trs = table.css("tr")
+            for tr in trs:
+                attr = tr.css("td.ttl a ::text").extract()
+                nfo = tr.css("td.nfo a ::text").extract()
+                if not nfo:
+                    nfo = tr.css("td.nfo ::text").extract()
+                if attr:
+                    if nfo:
+                        date[tit[0]][attr[0]] = nfo[0]
+                    else:
+                        date[tit[0]][attr[0]] = ""
+                elif nfo:
+                    date[tit[0]][nfo[0]] = nfo[0]
+
+        fo = open("Info/" + brand + ".json", "a")
+        json.dump(date, fo)
+        fo.close()
